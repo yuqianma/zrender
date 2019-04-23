ead0ed0e7a618d188c6932893285ecaa693ddc03
yuqianma <mayq0422@gmail.com>
2019-04-23 17:26:07 +0800

    prepare van sample

---
+++ test/van/container.js
@@ -0,0 +1,47 @@
+requireES([
+    'src/zrender.js',
+    'src/graphic/IncrementalDisplayable',
+    'src/graphic/shape/Circle',
+], function(
+    zrender,
+    IncrementalDisplayable,
+    Circle,
+) {
+    var zr = zrender.init(document.getElementById('main'));
+    window.zr = zr;
+
+    var w = zr.getWidth(), h = zr.getHeight();
+
+    window.addData = function () {
+        for(var i = 0; i < 1e5; i++) {
+            var circleShape = new Circle({
+                zlevel: 10,
+                position: [Math.random() * w, Math.random() *h],
+                shape: {
+                    r: 1
+                },
+                style: {
+                    fill: i > 500 ? '#0bd' : '#0b9',
+                },
+                incremental: true,
+            });
+            zr.add(circleShape);
+        }
+    };
+
+    addData();
+
+    zr.add(new Circle({
+        zlevel: 10,
+        position: [200, 200],
+        incremental: true,
+        shape: {
+            r: 10,
+        },
+        style: {
+            fill: 'red'
+        },
+        onmouseover: e => e.target.setShape('r', 20),
+        onmouseout: e => e.target.setShape('r', 10),
+    }));
+});
